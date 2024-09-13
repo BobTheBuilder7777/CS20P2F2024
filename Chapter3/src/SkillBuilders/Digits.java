54882503b181c08d88027042cc4f087aab8b9b0f
27204010 <27204010@L815WD220013.edu.cbe.ab.ca>
2024-09-13 10:23:26 -0600

    10:20 AM

---
+++ Chapter3/src/SkillBuilders/Digits.java
@@ -0,0 +1,21 @@
+package SkillBuilders;
+
+import java.util.*;
+
+public class Digits {
+	
+	public static void main(String[] args) 
+	{
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.print("Please enter a 2 digit number: ");
+		int user_numb = userInput.nextInt();
+	
+		
+		
+		System.out.print("The tens place digit is: ");
+		System.out.print("The ones place digit is: ");
+		
+	}
+
+}
