b314c9d379d9be6998b0d1667573bbaf39dc134d
27204010 <27204010@L815WD220013.edu.cbe.ab.ca>
2024-09-18 11:53:28 -0600

    11:53 AM

---
+++ Chapter3/src/Mastery/ProjectMastery.java
@@ -0,0 +1,26 @@
+package Mastery;
+
+import java.util.Scanner;
+
+public class ProjectMastery {
+
+	public static void main(String[] args) 
+	{
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.print("How much time was spent designing the project application?");
+		int designing = userInput.nextInt();		
+		
+		System.out.print("How much time was spent coding the project application?");
+		int coding = userInput.nextInt();		
+		
+		System.out.print("How much time was spent debugging the project application?");
+		int debugging = userInput.nextInt();		
+		
+		System.out.print("How much time was spent testing the project application?");
+		int testing = userInput.nextInt();		
+		
+		
+	}
+
+}
