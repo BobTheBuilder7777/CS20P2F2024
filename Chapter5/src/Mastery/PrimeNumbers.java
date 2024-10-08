3fdf9e261013810f6ee3f617573977fe13652c4f
27204010 <27204010@L815WD220013.edu.cbe.ab.ca>
2024-10-08 10:17:32 -0600

    10:18

--- Chapter5/src/Mastery/PrimeNumbers.java
+++ Chapter5/src/Mastery/PrimeNumbers.java
@@ -23,17 +23,46 @@ public class PrimeNumbers {
 		//Preparing for user input
 		Scanner input = new Scanner(System.in);
 				
-		//Declaration for usernumber 
-		int user_num;
+		//Declaration 
+		int user_num; //user input
+		int i; //for loop var
+		boolean prime = false; //to check if prime or not
 		
-		System.out.print("Enter a number. This program will tell you if it is prime or not!"); //prompt user to enter number
+		System.out.println("Enter a number. This program will tell you if it is prime or not: "); //prompt user to enter number
 		user_num = input.nextInt(); //store number in variable
 		
 		
-		if (user_num == 1 || user_num == 0 || user_num < 0)
+		
+		if (user_num == 1 || user_num == 0) //If the users inputed number is 1, 0 , or a negative number
 		{
+			prime = true; 
+		
+		} 
+		
+	
+		for (i = 2; i <= user_num / 2; ++i)
+		{
+			if (user_num % i == 0) //if the remainder is 0, than that means the number is not a prime
+				
+			{
+				prime = true;
 			
+			}
 		}
+		
+		if (!prime) //if prime is not true
+		{
+			System.out.print("Your number is prime.");
+		}
+		else //if prime is true
+		{
+			System.out.print("Your number isn't prime.");
+		}
+		
 	}
 
 }
+	
+
+		
+
