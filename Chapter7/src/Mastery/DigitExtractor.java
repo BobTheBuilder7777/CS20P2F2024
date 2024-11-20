55436b38d06aeb43ac11775bc8769d6c1c496a80
27204010 <27204010@L815WD220013.edu.cbe.ab.ca>
2024-11-20 11:57:15 -0700

    11:57

--- Chapter7/src/Mastery/DigitExtractor.java
+++ Chapter7/src/Mastery/DigitExtractor.java
@@ -1,9 +1,11 @@
+
+
 package Mastery;
 
 public class DigitExtractor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- Chapter7/src/Mastery/MySavings.java
+++ Chapter7/src/Mastery/MySavings.java
@@ -1,3 +1,16 @@
+/*
+
+Program: MySavings.java          Date: November 20th, 2024
+
+Purpose: Create a MySavings application that displays a menu of choices for entering pennies, nickels, dimes, and quarters into a piggy bank and then prompts the user to make a selection.
+
+Author: Rishi Bhalla 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+
 package Mastery;
 
 public class MySavings {
