b83a61824bd8657bfb9b9d0af2ee5507150ee76c
27204010 <27204010@L815WD220013.edu.cbe.ab.ca>
2024-11-21 10:23:19 -0700

    10:23

--- Chapter7/src/Mastery/PiggyBank.java
+++ Chapter7/src/Mastery/PiggyBank.java
@@ -2,5 +2,14 @@ package Mastery;
 
 public class PiggyBank {
 
-	public static double 
+	public static double penny, dimes, nickels, quarters; //creates public variables accessible outside this class
+	
+	public PiggyBank() { //Piggyback object which creates each variable initially as 0 since there isn't anything in the piggybank yet
+		penny = 0; 
+		nickels = 0;
+		dimes = 0;
+		quarters = 0;
+		
+	}
+	
 }
