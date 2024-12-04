8fca1774b84d50373906c9765225b5b635cf740e
27204010 <27204010@L815WD220013.edu.cbe.ab.ca>
2024-12-04 11:58:13 -0700

    11:58

--- Chapter7/src/Mastery/Food.java
+++ Chapter7/src/Mastery/Food.java
@@ -4,50 +4,56 @@ import java.text.DecimalFormat;
 
 public class Food {
 
-	DecimalFormat format = new DecimalFormat("#.##");
 	
-	private double hamburgers;
-	private double salads;
-	private double FF;
-	private double sodas;
-	private double order;
+	public static double order;
+	public String itemNa;
+	public int fat, fiber, carb;
 	
-	public Food() {
-		hamburgers = 0;
-		salads = 0;
-		FF = 0;
-		sodas = 0;
-		order = 0;
+	public Food(String item, double price, int fats, int carbs, int fibers) {
+		
+		order = price;
+		itemNa = item;
+		fat = fats;
+		fiber = fibers;
+		carb = carbs;
 	}
 	
-	public void burgerPrice(double burger) {
-		hamburgers = burger * 1.85;
-		System.out.println("Each hamburger has 9.0g of fat, 33.0g of carbs, and 1g of fiber.");
-		System.out.println("");
-	}
 	
-	public void saladPrice(double salad) {
-		salads = salad * 2;
-		System.out.println("Each salad has 1.0g of fat, 11.0g of carbs, and 5g of fiber.");
-		System.out.println("");
+
+
+
+	public String toString() {
+		String information;
+		information = "Each " + itemNa + " has " + fat + "g of fat, " + carb + "g of carbs, and " + fiber + "g of fiber.";
+		return information; //returns 
+		
+	} //this returns
+	
+	public static double getPrice(double burger, double FF, double salad, double soda) {
+	
+	
+		order = burger * 1.85;
+		order = order + (salad * 2);
+		order = order + (FF * 1.3);
+		order = order + (soda * 0.95);
+		return order;
 		
 	}
-	public void ffPrice(double Ff) {
-		FF = Ff * 1.30;
-		System.out.println("French Fries have 11.0g of fat, 36.0g of carbs, and 4g of fiber.");
-		System.out.println("");
-	}
-	public void sodaPrice(double Soda) {
-		sodas = Soda * 0.95;
-		System.out.println("Each soda has 0.0g of fat, 38.0g of carbs, and 0g of fiber.");
-		System.out.println("");
-	}
+
+
+
+
+
+
+
 	
-	public void Order() {
-		order = sodas + FF + salads + hamburgers;
-		System.out.println("");
-		System.out.println("Your total comes out to be: $" + format.format(order));
-	}
+
+
+
+
+
+
+
 
 	
 
--- Chapter7/src/Mastery/LunchOrder.java
+++ Chapter7/src/Mastery/LunchOrder.java
@@ -1,5 +1,6 @@
                                                                                                                                                                                                                                                                                                                                             package Mastery;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class LunchOrder {
@@ -9,27 +10,39 @@ public class LunchOrder {
 		Scanner input = new Scanner(System.in);
 		// import for user input
 		
-		Food food = new Food();
+		DecimalFormat format = new DecimalFormat("#.##"); //proper formating 
+		
+		
+		
+		//Setting the perameters
+		Food burgers = new Food("burger", 1.85, 9, 33, 1);
+		Food fries = new Food("fries", 1.3, 11, 36, 4);
+		Food salads = new Food("salads", 2, 1, 11, 5);
+		Food sodas = new Food("soda", 0.95, 9, 33, 1);
+		
 		int burger, salad, Ff, soda;
 		
-		System.out.print("Enter the number of hamburgers you want: ");
+		System.out.println("Enter the number of hamburgers you want: ");
 		burger = input.nextInt();
-		food.burgerPrice(burger);
+		System.out.println(burgers.toString());
 			
 		
-		System.out.print("Enter the number of salad you want: ");
+		System.out.println("Enter the number of salad you want: ");
 		salad = input.nextInt();
-		food.saladPrice(salad);
+		System.out.println(salads.toString());
 		
-		System.out.print("Enter the number of FF you want: ");
+		System.out.println("Enter the number of FF you want: ");
 		Ff = input.nextInt();
-		food.ffPrice(Ff);
+		System.out.println(fries.toString());
 		
-		System.out.print("Enter the number of soda you want: ");
+		System.out.println("Enter the number of soda you want: ");
 		soda = input.nextInt();
-		food.sodaPrice(soda);
+		System.out.println(sodas.toString());
 		
-		food.Order();
+		System.out.println("");
+		
+		System.out.println("Your order comes out to be: $" + format.format(Food.getPrice(Ff, salad, soda, burger)));
+		
 		
 		
 		
